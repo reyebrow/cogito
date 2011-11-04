a7aefa28b0ba9e683968a0726ff5f15f1c163700
Matt Reimer <matt@raisedeyebrow.com>
2011-11-04 14:20:13 -0700

    First commit is Boron on its own

---
+++ README.txt
@@ -0,0 +1,35 @@
+Boron is a base theme which converts the core template files to HTML5 markup.
+Like other minimal base themes, such as Zen and Stark, it includes only a few
+lines of layout-driven CSS, and markup that is search engine optimized.
+
+Unlike the others, Boron utilizes the new HTML5 doctype and semantic elements,
+such as Article, Header, Footer, and Section. The result is a
+standards-compliant theme with valid markup and minimal CSS that can be used
+as a jumping off point for developing new sub-themes. This leaves your site
+positioned to take advantage of new HTML5 features as they come into vogue.
+
+PHILOSOPHY
+The goal of developing this theme was to convert the core Drupal
+templates to HTML5. Wherever possible, core markup, attributes and class names
+were left intact, and elements were simply converted to their HTML5
+equivalents. This should make development easier, since this theme follows the
+standards and philosophies established by the core templates.
+
+FEATURES
+ - HTML5 doctype and meta content-type
+ - Header and Footer sections marked up with header and footer elements
+ - Navigation marked up with nav elements
+ - Sidebars marked up with aside elements
+ - Nodes marked up with article elements containing header and footer elements
+ - Comments marked up as articles nested within their parent node article
+ - Blocks marked up with section elements
+ - HTML5 shim script included for full IE compatibility
+ - Search form uses the new input type="search" attribute
+ - WAI-ARIA accessibility roles added to primary elements
+ - Updates all core modules to HTML5 markup
+
+DEVELOPERS
+The Boron theme was created by Scott Vandehey (spaceninja) of Metal
+Toad Media. Special thanks to Dylan Tack (grendzy) for code review and getting
+the theme posted here on drupal.org. Also thanks to our CEO, Joaquin
+Lippincott (metaltoad) for supporting this project.
---
+++ template.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @file
+ * Contains theme override functions and preprocess functions for the Boron theme.
+ */
+
+/**
+ * Changes the default meta content-type tag to the shorter HTML5 version
+ */
+function boron_html_head_alter(&$head_elements) {
+  $head_elements['system_meta_content_type']['#attributes'] = array(
+    'charset' => 'utf-8'
+  );
+}
+
+/**
+ * Changes the search form to use the HTML5 "search" input attribute
+ */
+function boron_preprocess_search_block_form(&$vars) {
+  $vars['search_form'] = str_replace('type="text"', 'type="search"', $vars['search_form']);
+}
+
+/**
+ * Uses RDFa attributes if the RDF module is enabled
+ * Lifted from Adaptivetheme for D7, full credit to Jeff Burnz
+ * ref: http://drupal.org/node/887600
+ */
+function boron_preprocess_html(&$vars) {
+  if (module_exists('rdf')) {
+    $vars['doctype'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">' . "\n";
+    $vars['rdf']->version = 'version="HTML+RDFa 1.1"';
+    $vars['rdf']->namespaces = $vars['rdf_namespaces'];
+    $vars['rdf']->profile = ' profile="' . $vars['grddl_profile'] . '"';
+  } else {
+    $vars['doctype'] = '<!DOCTYPE html>' . "\n";
+    $vars['rdf']->version = '';
+    $vars['rdf']->namespaces = '';
+    $vars['rdf']->profile = '';
+  }
+}
+
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return
+ *   A string containing the breadcrumb output.
+ */
+function boron_breadcrumb($vars) {
+  $breadcrumb = $vars['breadcrumb'];
+  // Determine if we are to display the breadcrumb.
+  $show_breadcrumb = theme_get_setting('breadcrumb_display');
+  if ($show_breadcrumb == 'yes') {
+
+    // Optionally get rid of the homepage link.
+    $show_breadcrumb_home = theme_get_setting('breadcrumb_home');
+    if (!$show_breadcrumb_home) {
+      array_shift($breadcrumb);
+    }
+
+    // Return the breadcrumb with separators.
+    if (!empty($breadcrumb)) {
+      $separator = filter_xss(theme_get_setting('breadcrumb_separator'));
+      $trailing_separator = $title = '';
+
+      // Add the title and trailing separator
+      if (theme_get_setting('breadcrumb_title')) {
+        if ($title = drupal_get_title()) {
+          $trailing_separator = $separator;
+        }
+      }
+      // Just add the trailing separator
+      elseif (theme_get_setting('breadcrumb_trailing')) {
+        $trailing_separator = $separator;
+      }
+
+      // Assemble the breadcrumb
+      return implode($separator, $breadcrumb) . $trailing_separator . $title;
+    }
+  }
+  // Otherwise, return an empty string.
+  return '';
+}
