f767a2062af45d012b6b33a75973e37979eb462a
Matt Reimer <matt@raisedeyebrow.com>
2012-01-04 16:28:36 -0800

    Cleaning up the regions template

--- templates/region.tpl.php
+++ templates/region.tpl.php
@@ -1,3 +1,12 @@
-<?php if ($content): ?>
-     <?php print $content; ?>
-  <?php endif; ?>
+<?php
+/**
+ * @file
+ * Default theme implementation to display a region
+ *
+ * Drupal uses WAY too much markup so we want to keep this simple: If region, print region
+ *
+ **/
+ 
+if ($content){
+  print $content;
+}
