b9bba2e8109259ea1b7a5302292fade361d68c9a
Matt Reimer <matt@raisedeyebrow.com>
2012-01-24 11:57:35 -0800

    Working on the starter child and migrating our wireframe content back into cogito

--- js/orbit-load.js
+++ js/orbit-load.js
@@ -22,7 +22,7 @@
       
       $('#featured').orbit({
         animation: 'fade', // fade, horizontal-slide, vertical-slide, horizontal-push
-        animationSpeed: 800, // how fast animtions are
+        animationSpeed: 100, // how fast animtions are
         timer: true, // true or false to have the timer
         advanceSpeed: 4000, // if timer is enabled, time between transitions
         pauseOnHover: false, // if you hover pauses the slider
@@ -30,7 +30,7 @@
         startClockOnMouseOutAfter: 1000, // how long after MouseOut should the timer start again
         directionalNav: true, // manual advancing directional navs
         captions: true, // do you want captions?
-        captionAnimation: 'fade', // fade, slideOpen, none
+        captionAnimation: 'slideOpen', // fade, slideOpen, none
         captionAnimationSpeed: 800, // if so how quickly should they animate in
         bullets: false, // true or false to activate the bullet navigation
         bulletThumbs: false, // thumbnails for the bullets
